8e604e5a14c589a454669e1283faa77fb73732fc
Alexalvesbh <alexalvesbh@gmail.com>
2018-03-26 07:35:29 -0300

    Classe Jogador

---
+++ Truco/src/br/com/pa/modelo/dominio/Jogador.java
@@ -0,0 +1,11 @@
+package br.com.pa.modelo.dominio;
+
+public class Jogador {
+	
+	public static String AGENTE_HUMANO = "JOGADOR";
+	public static String AGENTE_COMPUTADOR = "COMPUTADOR";
+	
+	private String tipoAgente;
+	
+	private Carta[] jogo;
+}
